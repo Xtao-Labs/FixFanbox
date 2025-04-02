a994f1122342d28411a4e720123deb9f90c588a1
xtaodada <xtao@xtao.de>
2025-04-02 15:54:22 +0800

    fix: vercel

--- src/route/article.py
+++ src/route/article.py
@@ -4,21 +4,33 @@ from starlette.responses import HTMLResponse
 
 from .base import get_redirect_response
 
+from src.api.httpxrequest import HTTPXRequest
+from src.api.render import RenderArticle
+
+from src.env import env_config
 from src.core.web_app import WebApp
 from src.error import ArticleError, ResponseException
 from src.log import logger
-from ..api.render import RenderArticle
 
 
 class ArticlePlugin(AsyncInitializingComponent):
     def __init__(self, web_app: WebApp, render: RenderArticle):
-        self.render = render
+        self._render = render
         web_app.app.add_api_route("/@{username}/posts/{post_id}", self.parse_article)
         web_app.app.add_api_route("/posts/{post_id}", self.parse_article)
         web_app.app.add_api_route(
             "/@{username}/posts/{post_id}/json", self.parse_article_json
         )
         web_app.app.add_api_route("/posts/{post_id}/json", self.parse_article_json)
+
+    @property
+    def render(self) -> "RenderArticle":
+        if env_config.START_WEB:
+            return self._render
+        h = HTTPXRequest()
+        self._render.fanbox_api.request = h
+        self._render.kemono_api.request = h
+        return self._render
 
     async def parse_article(self, post_id: str, request: Request):
         try:
